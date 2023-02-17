143dd99acab45507b92fad568db2cc9881d3c1f6
Moiz-ur-Rehman-03 <moiz.urrehman@tintash.com>
2023-02-17 17:23:18 +0500

    setting change

--- notebackend/settings.py
+++ notebackend/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6$d0p6r41^bj@$b20w=js)_7+o&+_nd(@nymkzy8%_9(++c^7^'
+SECRET_KEY = env('SECRET_KEY_DJANGO')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
